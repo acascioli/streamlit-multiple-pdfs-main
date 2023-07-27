45037c1866d65acaf2fed3a666a6b0e63333abfe
acascioli <alexcascioli@hotmail.it>
2023-07-27 13:08:20 +0200

    implemented multiple url scraping

--- app.py
+++ app.py
@@ -7,8 +7,35 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+
+
+def get_url_text(urls):
+    loader = UnstructuredURLLoader(
+        urls,
+    )
+    data = loader.load()
+    text_splitter = CharacterTextSplitter(
+        separator="\n", chunk_size=1000, chunk_overlap=200
+    )
+
+    docs = text_splitter.split_documents(data)
+    return docs
+
+
+def get_docs_text(docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1200,
+        chunk_overlap=200,
+        length_function=len,
+    )
+
+    docs_chunks = text_splitter.split_documents(docs)
+    return docs_chunks
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -21,10 +48,7 @@ def get_pdf_text(pdf_docs):
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
+        separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
     )
     chunks = text_splitter.split_text(text)
     return chunks
@@ -37,37 +61,43 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 
+def get_vectorstore_docs(docs):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_documents(documents=docs, embedding=embeddings)
+    return vectorstore
+
+
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
+        llm=llm, retriever=vectorstore.as_retriever(), memory=memory
     )
     return conversation_chain
 
 
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
+    response = st.session_state.conversation({"question": user_question})
+    st.session_state.chat_history = response["chat_history"]
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(
+                user_template.replace("{{MSG}}", message.content),
+                unsafe_allow_html=True,
+            )
         else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(
+                bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True
+            )
 
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -83,22 +113,48 @@ def main():
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True
+        )
         if st.button("Process"):
             with st.spinner("Processing"):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
+                print(raw_text)
 
                 # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
+                print("")
+                print(text_chunks)
 
                 # create vector store
                 vectorstore = get_vectorstore(text_chunks)
 
                 # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+
+        st.subheader("Your URL")
+        url1 = st.text_input("insert an url...")
+        url2 = st.text_input("insert another url...")
+        if url2 != "":
+            urls = [url1, url2]
+        else:
+            urls = [url1]
+        print("urls")
+        print(urls)
+        print("")
+
+        if st.button("Process url"):
+            with st.spinner("Processing"):
+                # get pdf text
+                docs = get_url_text(urls)
+                print(docs)
+
+                # create vector store
+                vectorstore = get_vectorstore_docs(docs)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
